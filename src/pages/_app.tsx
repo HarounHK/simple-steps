7c40c25b169dcd1866251344ca08dcde40c5c7f9
Haroun Kassouri <harounkassouri368@gmail.com>
2024-12-04 12:35:22 +0000

    Deployment Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AuthProvider } from "../app/providers";
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
