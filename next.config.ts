b6e58505ff2bf92588c3e65d8af7963f0f4e38a0
Haroun Kassouri <harounkassouri368@gmail.com>
2024-12-04 13:15:34 +0000

    temp fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXTAUTH_URL:
-      process.env.VERCEL_ENV === "development"
-        ? "http://localhost:3000"
-        : process.env.VERCEL_ENV === "preview"
-        ? `https://${process.env.VERCEL_URL}`
-        : "https://simple-steps-i3gv79srg-haroun-kassouris-projects.vercel.app", 
-  },
+  /* config options here */
 };
 
 export default nextConfig;
